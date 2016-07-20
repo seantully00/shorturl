0564be53f985a4ba56f9005db053b70acecd7e00
Sean <seantully00@hotmail.com>
2016-07-20 02:22:47 +0000

    setting up

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 'use strict';
 
+//Express
 var express = require('express');
 var app = express();
+
+//MongoDB
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = process.env.MONGOLAB_URI;
+
+//Define port
+var port = process.env.PORT || 8080;
+
+
+//Jade
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+//Listen on port
+app.listen(port, function () {
+  console.log('App listening on port ' + port + '!');
+});
