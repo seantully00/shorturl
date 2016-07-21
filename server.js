6e89abad5e26dd3cc51064a12fd9fd37adbb0675
Sean <seantully00@hotmail.com>
2016-07-21 02:53:58 +0000

    urltest

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var logger = require('morgan');
-var key = 1;
+var key = 0;
 
 //Express
 var express = require('express');
@@ -20,14 +20,22 @@ var urlSchema = mongoose.Schema({
 });
 
 
+function urltest(url) {
+    var a = "www.";
+    var b = "http";
+    if (url.substring(0, a.length) != a || url.substring(0, b.length) != b) {
+        console.log("Error: Not an acceptable url");
+    }
+}
+
 //Get URLs
 app.get('/new/:origurl', function(req, res) {
     var origurl = req.params.origurl;
     var newurl = "https://stshorturl.herokuapp.com/" + key;
-    key = key + 1;
     var doc = {'origurl': origurl, 'newurl': newurl, 'key': key};
     conn.collection('urls').insert(doc);
     res.json(doc);
+    key = key + 1;
 });
 
 //Pull URLs
