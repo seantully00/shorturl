ec82950a8a73cb03b0f95a1f310fd583957c57c4
Sean <seantully00@hotmail.com>
2016-07-25 00:46:49 +0000

    pull url

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ function urltest(testurl) {
 }
 
 //Get URLs
+/*
 app.get('/new/:origurl', function(req, res) {
     var origurl = req.params.origurl;
     var newurl = "https://stshorturl.herokuapp.com/" + key;
@@ -41,6 +42,7 @@ app.get('/new/:origurl', function(req, res) {
     res.json(doc);
     key = key + 1;
 });
+*/
 
 //Pull URLs
 app.get('/:number', function(req, res) {
