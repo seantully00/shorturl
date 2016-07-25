52db5c10f712450c42147718928e293e53dce607
Sean <seantully00@hotmail.com>
2016-07-25 01:52:51 +0000

    post instead of get

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ function urltest(testurl) {
 
 //Get URLs
 
-app.get('/new/:origurl', function(req, res) {
+app.post('/new/:origurl', function(req, res) {
     var origurl = req.params.origurl;
     var newurl = "https://stshorturl.herokuapp.com/" + key;
     urltest(origurl);
