36767aec6c063665a943b959ef1073459ebac2e3
Sean <seantully00@hotmail.com>
2016-07-25 00:34:46 +0000

    pull url

--- server.js
+++ server.js
@@ -48,8 +48,10 @@ app.get('/:number', function(req, res) {
     coll.findOne({'key': number}, function(err, site) {
         if (err) {console.log(err); res.redirect("/");} 
         else {
-            //res.redirect(site.origurl);
-            res.redirect("/");
+            console.log('Found URL')
+            console.log(site.origurl);
+            res.redirect(site.origurl);
+            //res.redirect("/");
         }
     });
 });
